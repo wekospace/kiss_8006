1c8d4272efac3c0f66f0cbf55f6687cedc80c28c
Romuald Conty <neomilium@gmail.com>
2016-02-06 21:35:52 +0000

    Add KmmPi class to ease setup on Raspberry Pi

---
+++ pyk8006/kmm/pi/__init__.py
@@ -0,0 +1,28 @@
+#!/bin/bin/env python3
+
+from .. import Kmm
+
+from RPi import GPIO
+
+class KmmPi(Kmm):
+    def __init__(self, irq_pin, debug=False):
+        Kmm.__init__(self, async=True, debug=False)
+        self.__irq_pin = irq_pin
+
+        def irq_pin_trigged(pin):
+            if debug: print("IRQ on:" + str(pin))
+            self.request_reception()
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(irq_pin, GPIO.IN)
+        GPIO.add_event_detect(irq_pin, GPIO.FALLING, callback=irq_pin_trigged)
+
+        self.check_irq_pin()
+
+    def read_irq_pin(self):
+        return GPIO.input(self.__irq_pin)
+
+    def check_irq_pin(self):
+        while(self.read_irq_pin() == 0):
+            self.request_reception()
+
---
+++ pyk8006/tests/test_pi_buttons.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+sys.path.append('../')
+from kmm import *
+from kmm.pi import KmmPi
+
+debug=False
+
+fp = KmmPi(irq_pin=22,debug=debug)
+
+def int_to_chr(i):
+    if(i == 0xaa):
+        return '0xaa'
+    else:
+        return chr(i)
+
+def button_received(button):
+    print(button)
+    fp.set_text(str(button))
+    fp.check_irq_pin()
+
+fp.register_input_button_callback(button_received)
+
+fp.set_text('Hit a key')
+
+while True:
+    time.sleep(1)
+    #print "I'm alive !"
+    input = fp.read_irq_pin()
+    if input != 1: print("Error: IRQ pin value is " + str(GPIO.input(irq)))
