d89ca49786440eceedae224bca0d3da69ef9a14f
Romuald Conty <neomilium@gmail.com>
2016-02-07 21:12:38 +0000

    Rework input event handling
    Add a dedicated decoder class.
    
    Detect pressed and hold keys and report them as it with timestamp and duration when relevant (ie. hold)

--- pyk8006/kmm/__init__.py
+++ pyk8006/kmm/__init__.py
@@ -31,6 +31,7 @@ class Kmm():
             self.__io = kmm_io_thread.KmmIoThread(debug)
             self.__io.start()
             self.__send_raw_data = self.__io.put_tx
+            self.__kied = KmmInputEventDecoder(0.12)
 
     def send_raw_payload(self, payload):
         header = [0x23, 0x00, 0x00]
@@ -134,7 +135,9 @@ class Kmm():
     def __frame_received(self, frame):
         if(self.__input_button_callback != None):
             payload = bytes(frame[3:]).decode()
-            self.__input_button_callback(mapping.payload_to_code(payload))
+            kie = self.__kied.decode(payload)
+            if kie != None:
+                print(str(kie))
 
     def __del__(self):
         if self.__async: self.__io.terminate()
@@ -144,3 +147,60 @@ from enum import Enum
 class LedColor(Enum):
     (off, red, orange, green) = range(4)
 
+class InputEventType(Enum):
+    (pressed, hold, released) = range(3)
+
+import datetime
+
+class KmmInputEventDecoder:
+    def __init__(self, repeat_delta):
+        self.__last_received_ir_input = None
+        self.__last_pressed_ir_input = None
+        self.__last_transmitted_ir_input = None
+        self.__repeat_delta = repeat_delta
+
+    def decode(self, payload):
+        if(payload[0:2] == 'Ir'):
+            return self.__decode_ir(payload[2:])
+        elif(payload[0:1] == 'B'):
+            return self.__decode_button(payload[1:])
+        else:
+            raise NamedError('Unknown reply type')
+
+    def __decode_ir(self, ir_code):
+        kie = None
+        ir_input = (ir_code, datetime.datetime.now())
+        ir_key = mapping.ir_code_to_key(ir_code)
+
+        if self.__last_received_ir_input == None or self.__last_received_ir_input[0] != ir_input[0]:
+            self.__last_pressed_ir_input = ir_input
+            self.__last_transmitted_ir_input = ir_input
+            kie = KmmInputEvent(ir_key, InputEventType.pressed, ir_input[1])
+        else:
+            delta = (ir_input[1]-self.__last_received_ir_input[1]).total_seconds()
+            if delta > 0.12: # Key have been released then pressed again
+                self.__last_pressed_ir_input = ir_input
+                self.__last_transmitted_ir_input = ir_input
+                kie = KmmInputEvent(ir_key, InputEventType.pressed, ir_input[1])
+            else:
+                repeat_delta = (ir_input[1]-self.__last_transmitted_ir_input[1]).total_seconds()
+                if repeat_delta > self.__repeat_delta:
+                    duration = (ir_input[1]-self.__last_pressed_ir_input[1]).total_seconds()
+                    self.__last_transmitted_ir_input = ir_input
+                    kie = KmmInputEvent(ir_key, InputEventType.hold, ir_input[1], duration)
+
+        self.__last_received_ir_input = ir_input
+        return kie
+
+    def __decode_button(self, button_code):
+        raise NamedError('Implement me!')
+
+class KmmInputEvent:
+    def __init__(self, key, type, timestamp, duration=0.0):
+        self.timestamp = timestamp
+        self.key = key
+        self.type = type
+        self.duration = duration
+
+    def __str__(self):
+        return('InputEvent(key="'+self.key+'", type="'+str(self.type)+'", timestamp="'+str(self.timestamp)+'", duration="'+str(self.duration)+'")')
